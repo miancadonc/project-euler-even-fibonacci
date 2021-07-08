f57f66cf9d555a73860c1e322b20e05363a6fccb
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-07-07 19:29:27 -0500

    commit tree 9883f17d92a4596e17203ffb4451dd65774aee06

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+    arr = [1, 2]
+
+    while arr[-1] < limit do
+        new_term = arr[-1] + arr[-2]
+        arr.push(new_term)
+    end
+
+    arr.pop
+
+    arr.select{|i| i.even?}.sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def sum
+        arr = [1, 2]
+
+        while arr[-1] < @limit do
+            new_term = arr[-1] + arr[-2]
+            arr.push(new_term)
+        end
+
+        arr.pop
+
+        arr.select{|i| i.even?}.sum
+    end
+end
